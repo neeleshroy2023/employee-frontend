81caa69b1281ff4988198e22e6a318959209b9a1
Rahul <rahulpratik1212@gmail.com>
2025-02-11 00:59:03 +0530

    add feed

--- src/components/pages/Employees.tsx
+++ src/components/pages/Employees.tsx
@@ -1,7 +1,12 @@
 import { Box, Typography } from "@mui/material";
 import EmployeeList from "../features/employees/EmployeeList";
+import useFeed from "../../hooks/feature/useFeed";
 
 const Employees = () => {
+
+  const employees = useFeed()
+  console.log(employees);
+
   return (
     <Box>
       <Typography variant="h2">Employees</Typography>
