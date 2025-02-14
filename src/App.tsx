80e0068f73e34e3c7e4dd8eeadc6879702313fa9
Prajwal V <prajuu2000@gmail.com>
2025-02-14 11:49:10 +0530

    added infinite scroll

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router";
 import Router from "./components/features/navigation/Router";
 import Navbar from "./Ui/Navbar";
 import { UserProvider } from "./Context/UserContext";
+// import InfoPage from "./infiniteScroll/InfoPage";
 function App() {
   return (
     <ThemeProvider theme={theme}>
@@ -15,6 +16,7 @@ function App() {
           <div className="App">
             <Container maxWidth="xl">
               <Router />
+              {/* <InfoPage/> */}
             </Container>
           </div>
         </BrowserRouter>
